ab57d429a56c864c0e62551e1302c2dd046c5bc6
Thawan Camara <thawan@debian-host.tde-souz.42.fr>
2023-12-03 03:06:28 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Header from './components/Header';
+import SideNav from './components/SideNav';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <SideNav />
+    </div>
+  );
+}
+
+export default App;
